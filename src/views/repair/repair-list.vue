391f9cbbc3e1e32b431a70dfa3b83f2e29c54901
YJZHOU <321403613@qq.com>
2024-09-24 16:30:47 +0800

    【报修功能开发】完善基本的功能开发

--- src/views/repair/repair-list.vue
+++ src/views/repair/repair-list.vue
@@ -1,29 +1,20 @@
 <template>
   <div>
-    <!-- <el-input placeholder="请输入申请人搜索" v-model="searchQuery" clearable @input="filterData" style="margin-bottom: 20px; width: 300px;">
-    </el-input> -->
-
     <el-form :inline="true" :model="searchForm" class="search-form">
       <el-form-item label="申请人">
-        <el-input v-model="searchForm.proposer" placeholder="请输入申请人名称"></el-input>
+        <el-input v-model="searchForm.replyName" placeholder="请输入申请人名称"></el-input>
       </el-form-item>
-
       <el-form-item label="任务状态">
         <el-select v-model="searchForm.jobStatusList" placeholder="请选择任务状态" multiple>
-          <el-option label="提交" value="1">
-          </el-option>
-          <el-option label="接单" value="2"></el-option>
-          <el-option label="处理中" value="3">
-          </el-option>
-          <el-option label="完成" value="4"></el-option>
-          <el-option label="确认" value="5"></el-option>
-          <el-option label="取消任务" value="6"></el-option>
-          <el-option label="取消接单" value="7"></el-option>
+          <el-option label="待处理" value="1"></el-option>
+          <el-option label="处理中" value="2"></el-option>
+          <el-option label="已完成" value="4"></el-option>
+          <el-option label="已确认" value="5"></el-option>
+          <el-option label="已取消" value="6"></el-option>
         </el-select>
       </el-form-item>
-
       <el-form-item label="申请时间">
-        <el-date-picker v-model="searchForm.replyDate" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
+        <el-date-picker v-model="searchForm.dateRange" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
         </el-date-picker>
       </el-form-item>
       <el-form-item>
@@ -36,54 +27,70 @@
       申请报修
     </el-button>
 
-    <el-table :data="filteredTableData" style="width: 100%" height="250">
+    <el-table :data="tableData" style="width: 100%">
       <el-table-column fixed label="序号" width="50">
         <template slot-scope="scope">
           {{ scope.$index + 1 }}
         </template>
       </el-table-column>
-      <el-table-column prop="jobstatus" label="任务状态" width="120">
+      <el-table-column label="任务状态" width="130">
+        <template slot-scope="scope">
+          {{ getJobStatusName(scope.row.status) }}
+        </template>
       </el-table-column>
-      <el-table-column fixed prop="replyDate" label="申请时间" width="150">
+      <el-table-column prop="createTime" label="申请时间" width="150">
       </el-table-column>
-      <el-table-column prop="location" label="故障位置" width="120">
+      <el-table-column label="故障位置" width="130">
+        <template slot-scope="scope">
+          {{scope.row.floor+'-'+scope.row.corridor+'-'+scope.row.position}}
+        </template>
       </el-table-column>
-      <el-table-column prop="replyer" label="申请人" width="120">
+      <el-table-column prop="proposerName" label="申请人" width="130">
       </el-table-column>
-      <el-table-column prop="handler" label="处理人" width="120">
+      <el-table-column prop="handlerName" label="处理人" width="130">
       </el-table-column>
-      <el-table-column prop="description" label="描述" width="300">
+      <el-table-column prop="description" label="描述" width="280">
       </el-table-column>
-      <el-table-column label="图片" width="200">
+      <el-table-column prop="remark" label="备注" width="140">
+      </el-table-column>
+
+      <el-table-column label="图片" width="320">
         <template slot-scope="scope">
           <div style="display: flex; gap: 5px; justify-content: center;">
-            <img v-for="(img, index) in scope.row.images" :key="index" :src="img" alt="图片" style="width: 60px; height: auto;" @error="handleImageError">
+            <img v-for="(img, index) in scope.row.imagesList" :key="index" :src="`data:image/jpeg;base64,${img.fileData}`" alt="图片" style="width: 120px; height: auto; cursor: pointer;" @click="showImage(img.fileData)">
           </div>
         </template>
       </el-table-column>
+
+      <el-dialog :visible.sync="imageDialogVisible" width="50%">
+        <img :src="selectedImage" alt="放大图片" style="width: 100%;">
+      </el-dialog>
+
       <el-table-column label="操作" width="100">
         <template slot-scope="scope">
-          <el-button type="text" @click="jobOperation(scope.row.jobId,scope.row.jobstatusCode)" v-if="[1, 2, 3].includes(scope.row.jobstatusCode) " style="color:  #ff6666;">取消任务
+          <el-button type="text" @click="jobOperation(scope.row.id,scope.row.jobId,'cancel')" v-if="[1, 2, 3].includes(scope.row.status) " style="color:  #ff6666;">取消任务
           </el-button>
-          <el-button type="text" @click="jobOperation(scope.row.jobId,scope.row.jobstatusCode)" v-if="scope.row.jobstatusCode === 1" style="color: #4CAF50;">
+          <el-button type="text" @click="jobOperation(scope.row.id,scope.row.jobId,'deal')" v-if="scope.row.status === 1" style="color: #4CAF50;">
             开始处理
           </el-button>
-          <el-button type="text" @click="jobOperation(scope.row.jobId,scope.row.jobstatusCode)" v-else-if="scope.row.jobstatusCode === 2">
-            开始处理
-          </el-button>
-          <el-button type="text" @click="jobOperation(scope.row.jobId,scope.row.jobstatusCode)" v-else-if="scope.row.jobstatusCode === 3" style="color: #2196F3;">
+          <el-button type="text" @click="jobOperation(scope.row.id,scope.row.jobId,'finish')" v-else-if="scope.row.status === 2">
             完成
           </el-button>
-          <el-button type="text" @click="jobOperation(scope.row.jobId,scope.row.jobstatusCode)" v-else-if="scope.row.jobstatusCode === 4">
+
+          <el-button type="text" @click="jobOperation(scope.row.id,scope.row.jobId,'confirm')" v-else-if="scope.row.status === 4">
             确认
           </el-button>
-          <el-button type="text" @click="jobOperation(scope.row.jobId,scope.row.jobstatusCode)" v-else>
+          <!-- <el-button type="text" @click="jobOperation(scope.row.id,scope.row.jobId,'delete')" v-else>
             删除
-          </el-button>
+          </el-button> -->
         </template>
       </el-table-column>
 
     </el-table>
+
+    <!-- 分页组件 -->
+    <el-pagination background layout="total, sizes, prev, pager, next, jumper" :total="totalItems" :page-sizes="[10, 20, 50, 100]" :page-size="pageSize" :current-page.sync="currentPage" @current-change="handlePageChange">
+    </el-pagination>
 
     <!-- 申请维修组件 -->
     <repairReplyVue :visible="applyVisible" @handleClose="handleClose" @handleSubmit="handleSubmit"></repairReplyVue>
@@ -95,87 +102,122 @@ import repairReplyVue from './repair-reply.vue';
 
 
 export default {
+  mounted() {
+    console.log("开始加载数据")
+    this.fetchData(); // 加载第一页数据
+  },
   components: {
     repairReplyVue
 
   },
   data() {
     return {
-      searchForm: {
-        proposer: '',
-        jobStatusList: [],
-        replyDate: '',
-      },
+
+      // 查看图片对话框
+      imageDialogVisible: false,
+      selectedImage: '',
+
+      // 申请对话框
       applyVisible: false,
-      newEntry: {
-        date: '',
-        province: '',
-        city: '',
-        address: '',
-        images: []
+
+      // 表格数据
+      tableData: [],
+
+      // 搜索
+      searchForm: {
+        replyName: null,
+        statusList: [],
+        dateRange: []
       },
-      tableData: [{
-        jobId: '731093710',
-        jobstatus: '待处理',
-        jobstatusCode: 1,
-        replyDate: '2016-05-03 19:10',
-        location: 'b94f-01-03',
-        replyer: '周小舟',
-        handler: '王离',
-        description: '我是问题描述',
-        images: [
-          'https://example.com/image1.jpg',
-          'https://example.com/image2.jpg'
-        ]
-      }, {
-        jobstatus: '已安排',
-        jobstatusCode: 3,
-        replyDate: '2017-05-03 19:10',
-        location: 'b94f-01-03',
-        replyer: '周晓晓',
-        handler: '王离',
-        description: '我是问题描述2',
-        images: [
-          'https://example.com/image1.jpg',
-          'https://example.com/image2.jpg'
-        ]
-      }]
+
+      // 分页
+      totalItems: 0, // 总条目数
+      pageSize: 10,  // 每页条数
+      currentPage: 1, // 当前页码
     }
   },
   computed: {
-    filteredTableData() {
-      // 实现过滤逻辑
-      return this.tableData
-    }
   },
+
   methods: {
+
+    // 点击查看图片
+    showImage(fileData) {
+      this.selectedImage = `data:image/jpeg;base64,${fileData}`;
+      this.imageDialogVisible = true;
+      console.log(this.selectedImage)
+    },
+
+    // 获取表格数据 
+    fetchData() {
+      const params = {
+        pageNum: this.currentPage, // 当前页码
+        pageSize: 10,   // 每页显示的数据量
+        replyName: this.searchForm.replyName,
+        statusList: this.searchForm.jobStatusList,  // 状态列表
+        startDate: this.searchForm.dateRange
+          ? new Date(this.searchForm.dateRange[0]).toISOString().slice(0, 10) // 只保留日期
+          : null,
+        endDate: this.searchForm.dateRange
+          ? new Date(this.searchForm.dateRange[1]).toISOString().slice(0, 10) // 只保留日期
+          : null,
+
+
+      };
+      this.$http({
+        url: this.$http.adornUrl(`/job/repair/listRepairJob`),  // 接口地址
+        method: 'post',              //  POST 请求
+        data: params,                // 使用 data 传递参数
+      }).then((response) => {
+        console.log(params)
+        console.log(response)
+        const data = response.data.data;  // 解析后端返回的分页数据
+        this.tableData = data.list;       // 表格数据
+        this.totalItems = data.total;     // 总条目数
+        this.currentPage = data.pageNum;  // 当前页码
+        this.pageSize = data.pageSize;    // 每页显示条数
+      }).catch((error) => {
+        console.log('获取数据失败：', error);
+      });
+    },
+
+    handlePageChange(page) {
+      this.currentPage = page;
+      this.fetchData(); // 切换页码时重新获取数据
+    },
+
+    getJobStatusName(jobStatusCode) {
+      if (jobStatusCode == 1) {
+        return "待处理"
+      } else if (jobStatusCode == 2) {
+        return "处理中"
+      } else if (jobStatusCode == 4) {
+        return "已完成"
+      } else if (jobStatusCode == 5) {
+        return "已确认"
+      } else if (jobStatusCode == 6) {
+        return "已取消"
+      } else {
+        return "未知状态"
+      }
+    },
     handleClose() {
       this.applyVisible = false
     },
 
-    handleSubmit(data) {
-      console.log(data)
+    handleSubmit() {
+      this.$emit('flshboardData'); // 通知父组件刷新数据
       this.handleClose()
+      this.fetchData()
     },
     handleOpen() {
       this.applyVisible = true
     },
 
-
-    handleImageError(event) {
-      event.target.src = 'https://via.placeholder.com/60'; // 占位图链接
-    },
     openAddDialog() {
       this.applyVisible = true
     },
 
-    addNewEntry() {
-      this.tableData.push({ ...this.newEntry, images: this.newEntry.images.split(',') }); // 将图片链接转换为数组
-      this.addDialogVisible = false;
-    },
-    deleteEntry(index) {
-      this.tableData.splice(index, 1); // 删除指定索引的记录
-    },
 
     cancelJob(jobId) {
       console.log("任务：" + jobId + "已经取消")
@@ -184,34 +226,43 @@ export default {
     // 表单搜索功能
     search() {
       console.log("搜索条件:", this.searchForm);
-      // searchForm
+
+      this.fetchData()
 
     },
 
-    // operationButton(jobstatusCode) {
-    //   if (jobstatusCode === 1) {
-    //     return '接单';
-    //   } else if (jobstatusCode === 2) {
-    //     return '开始处理';
-    //   } else if (jobstatusCode === 3) {
-    //     return '完成';
-    //   } else if (jobstatusCode === 4) {
-    //     return '确认';
-    //   } else {
-    //     return '删除'; // 这里返回一个默认值
-    //   }
-    // },
     // 重置搜索框表单
     resetForm() {
       this.searchForm = {
-        proposer: '',
-        jobStatusList: [],
-        replyDate: '',
+        replyName: null,
+        statusList: [],
+        dateRange: []
       }
     },
-    jobOperation(jobId, jobstatusCode) {
+    jobOperation(id, jobId, operationType) {
       //调接口处理操作
-      console.log(jobId + jobstatusCode)
+      console.log(id + jobId + operationType)
+      const params = {
+        id: id,
+        jobId: jobId,
+        handlerId: this.$store.state.user.id,
+        operationType: operationType
+      };
+      this.$http({
+        url: this.$http.adornUrl(`/job/repair/changeStatus`),  // 接口地址
+        method: 'post',              // 改为 POST 请求
+        data: params,                // 使用 data 传递参数
+      }).then((response) => {
+        this.fetchData()
+        this.$emit('flshboardData'); // 通知父组件刷新数据
+        this.$message({
+          message: '操作成功',
+          type: 'success',
+          duration: 1000
+        });
+      }).catch((error) => {
+        console.log('操作任务状态失败：', error);
+      });
 
     }
 
--- src/views/repair/repair-reply.vue
+++ src/views/repair/repair-reply.vue
@@ -1,47 +1,62 @@
 <template>
   <el-dialog :title="'申请报修'" :close-on-click-modal="false" :visible.sync="dialogVisible" @open="handleOpen" @close="handleClose" width="30%">
     <div>
-      <el-form :model="dataForm" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="100px">
-        <el-form-item label="维修类型" prop="counterType">
-          <el-select v-model="dataForm.type" style="width:100%" placeholder="类型">
+      <el-form :model="dataForm" ref="dataForm" :rules="dataRule" @keyup.enter.native="handSubmit()" label-width="100px">
+        <el-form-item label="维修类型" prop="repairType">
+          <el-select v-model="dataForm.repairType" style="width:100%" placeholder="维修类型">
             <el-option v-for="op in type" :key="op.item" :label="op.value" :value="op.item" />
           </el-select>
         </el-form-item>
         <el-form-item label="申请部门" prop="department">
-          <el-input v-model="dataForm.location1" placeholder="位置"></el-input>
+          <el-input v-model="dataForm.department" placeholder="申请部门"></el-input>
         </el-form-item>
 
-        <el-row>
-          <el-col :span="6">
-            <el-form-item label="楼层" prop="floor" style="width: 180px;" filterable>
-              <el-select v-model="dataForm.location1" placeholder="请">
-                <el-option label="b91f" value="1"></el-option>
-                <el-option label="b92f" value="2"></el-option>
-                <el-option label="b93f" value="3"></el-option>
-                <el-option label="b94f" value="4"></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
+        <!-- <el-row> -->
+        <!-- <el-col :span="6"> -->
+        <el-form-item label="楼层" prop="floor" style="width: 180px;" filterable>
+          <el-select v-model="dataForm.floor" placeholder="">
+            <el-option label="b91f" value="b91f"></el-option>
+            <el-option label="b92f" value="b92f"></el-option>
+            <el-option label="b93f" value="b93f"></el-option>
+            <el-option label="b94f" value="b94f"></el-option>
+          </el-select>
+        </el-form-item>
+        <!-- </el-col> -->
 
-          <el-col :span="8">
-            <el-form-item label="位" prop="corridor">
-              <el-input v-model="dataForm.corridor"></el-input>
-            </el-form-item>
-          </el-col>
+        <!-- <el-col :span="8"> -->
+        <el-form-item label="位" prop="corridor">
+          <el-input v-model="dataForm.corridor"></el-input>
+        </el-form-item>
+        <!-- </el-col> -->
 
-          <el-col :span="8">
-            <el-form-item label="具体位置" prop="position">
-              <el-input v-model="dataForm.position"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
+        <!-- <el-col :span="8"> -->
+        <el-form-item label="具体位置" prop="position">
+          <el-input v-model="dataForm.position"></el-input>
+        </el-form-item>
+        <!-- </el-col>
+        </el-row> -->
 
-        <el-form-item label="描述" prop="remarks">
+        <el-form-item label="描述" prop="description">
           <el-input v-model="dataForm.description" placeholder="描述" type="textarea"></el-input>
         </el-form-item>
 
-        <el-form-item label="备注" prop="remarks">
-          <el-input v-model="dataForm.remarks" placeholder="备注"></el-input>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="dataForm.remark" placeholder="备注"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-upload class="upload-demo" 
+          :action="uploadAction" 
+          :on-preview="handlePreview" 
+          :on-remove="handleRemove" 
+          :on-success="handleSuccess" 
+          :on-error="handleError" 
+          :file-list="fileList" 
+          :data="uploadData()"
+           list-type="picture">
+            <el-button size="small" type="primary">上传图片</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过10m</div>
+          </el-upload>
         </el-form-item>
 
       </el-form>
@@ -49,7 +64,7 @@
       <div class="footer-container">
         <span slot="footer" class="dialog-footer">
           <el-button @click="handleCancel">取消</el-button>
-          <el-button type="primary" @click="handleSubmit">确定</el-button>
+          <el-button type="primary" @click="handSubmit()">确定</el-button>
         </span>
       </div>
     </div>
@@ -66,7 +81,15 @@ export default {
   },
   data() {
     return {
-      dialogVisible: false,
+      // 上传的图片列表
+      fileList: [],
+      // 上传图片的jobid
+      jobId: '',
+      // 上传图片的路径
+
+              
+      uploadAction: this.url = this.$http.adornUrl(`/job/repair/uploadImage?token=${this.$cookie.get('token')}`),
+      dialogVisible: this.visible,
       optionsCode: { 'bu_name': '1001', 'position_name': '1002', 'counter_type': '1003', 'counter_id': '1004' },
       type: [
         { item: '1', value: '我是维修类型1' },
@@ -74,48 +97,136 @@ export default {
         { item: '3', value: '我是维修类型3' }
       ],
       dataForm: {
-        id: 0,
-        counterId: '',
-        counterType: '',
-        location1: '',
-        location2: '',
-        location3: '',
-        image: '',
-        remarks: '',
+        repairType: null,
+        department: null,
+        floor: null,
+        corridor: null,
+        position: null,
+        description: null,
+        remark: null,
+      },
+      dataRule: {
+        department: [
+          { required: true, message: '部门编码不能为空', trigger: 'blur' }
+        ],
+        floor: [
+          { required: true, message: '楼层不能为空', trigger: 'blur' }
+        ],
+        corridor: [
+          { required: true, message: '位不能为空', trigger: 'blur' }
+        ],
+        position: [
+          { required: true, message: '具体位置不能为空', trigger: 'blur' }
+        ],
+        description: [
+          { required: true, message: '描述不能为空', trigger: 'blur' }
+        ]
+
       }
     };
   },
   watch: {
     visible(val) {
-      this.dialogVisible = val
+      this.dialogVisible = val;
     }
   },
   methods: {
-    handleOpen() {
+    initDataForm() {
+      this.dataForm.repairType = null;
+      this.dataForm.department = null
+      this.dataForm.floor = null
+      this.dataForm.corridor = null
+      this.dataForm.position = null
+      this.dataForm.description = null
+      this.dataForm.remark = null
+      this.fileList = [];
+      this.jobId = null
+    },
 
+    handleOpen() {
+      this.initDataForm()
+      this.jobId = Date.now();
     },
     handleClose() {
       this.$emit('handleClose')
     },
-    handleSubmit() {
-      this.$emit('handleSubmit', this.dataForm)
-    },
-
     handleCancel() {
+      this.initDataForm()
       this.handleClose()
     },
 
+    handleRemove(file, fileList) {
+      console.log("点击了删除图片");
 
-    dataFormSubmit() {
+      console.log(file);
+            console.log(fileList);
+
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleSuccess(response, file, fileList) {
+      console.log("图片上传成功!");
+      this.$message.success('图片上传成功!');
+      this.fileList = fileList;  // 更新文件列表
+    },
+    // 上传失败后的处理
+    handleError(error, file, fileList) {
+      this.$message.error('图片上传失败!');
+    },
+    // 上传图片的额外数据jobid
+    uploadData() {
+      if (!this.jobId) {
+        this.jobId = Date.now();
+      }
+      return { jobId: this.jobId};
+    },
+
+    handSubmit() {
       this.$refs.dataForm.validate((valid) => {
         if (valid) {
           console.log('提交数据：', this.dataForm);
-          this.visible = false; // 关闭对话框
+          console.log('jobId', this.jobId);
+
+          this.$http({
+            url: this.$http.adornUrl(`/job/repair/reply`),
+            method: 'post',
+            data: this.$http.adornData({
+              'jobId': this.jobId,
+              'department': this.dataForm.department,
+              'type': this.dataForm.repairType,
+              'description': this.dataForm.description,
+              'remark': this.dataForm.remark,
+              'floor': this.dataForm.floor,
+              'corridor': this.dataForm.corridor,
+              'position': this.dataForm.position,
+              'proposerId':this.$store.state.user.id,
+              'handlerId':this.$store.state.user.id
+            })
+          }).then(({ data }) => {
+            if (data && data.code === 200) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.$emit('handleSubmit'); // 通知父组件刷新数据
+                  this.dialogVisible = false; // 关闭对话框
+                }
+              });
+            } else {
+              console.error(data)
+              this.$message.error("提交失败");
+            }
+          }).catch(error => {
+            console.error('提交失败:', error);
+          });
         } else {
           console.log('表单验证失败！');
         }
       });
     }
+
   }
 }
 </script>
