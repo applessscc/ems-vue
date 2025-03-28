081252edbaf695eab5862d3ff29ea18e56df29ec
郑志宏 <2696122250@qq.com>
2025-03-28 11:31:38 +0800

    【代码提交】

--- src/views/air-condition/addKV.vue
+++ src/views/air-condition/addKV.vue
@@ -4,6 +4,23 @@
     <div class="logo-container">
       <span class="logo-text">VTech 温区配置</span>
     </div>
+    <div>
+
+      <el-dialog title="提示" :visible.sync="workCalendarDigStatus" width="30%">
+        <span>这是一段信息</span>
+
+
+        <el-calendar v-model="value" :day-style="dayStyle">
+        </el-calendar>
+
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="workCalendarDigStatus = false">取 消</el-button>
+          <el-button type="primary" @click="workCalendarDigStatus = false">确 定</el-button>
+        </span>
+      </el-dialog>
+
+      <el-butttton type="primary" @click="workCalendar()">工作日历</el-butttton>
+    </div>
     <div class="form-container">
       <el-form :model="form" label-width="60px" :inline="true">
 
@@ -22,6 +39,7 @@
             </el-option>
           </el-select>
         </el-form-item>
+
         <el-form-item label="ID">
           <el-select v-model="form.id" placeholder="请选择设备" clearable filterable style="width: 170px"
             :disabled="!form.sbu">
@@ -30,12 +48,18 @@
             </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="系数">
-          <el-input v-model="form.coefficient" style="width: 80px" placeholder="系数"
+
+        <el-form-item label="温度系数" label-width="100px">
+          <el-input v-model="form.coefficient" style="width: 100px" placeholder="温度系数"
             @input="validateNumber('coefficient')"></el-input>
         </el-form-item>
+
+        <el-form-item label="湿度系数" label-width="100px">
+          <el-input v-model="form.coefficientH" style="width: 100px" placeholder="湿度系数"
+            @input="validateNumber('coefficientH')"></el-input>
+        </el-form-item>
+
         <el-form-item>
-          <el-form-item></el-form-item>
           <el-button type="primary" @click="onSubmit" plain>添加</el-button>
           <el-button type="danger" @click="clean" plain>一键清空</el-button>
         </el-form-item>
@@ -44,15 +68,26 @@
 
     <div class="table-container">
       <el-table :data="tableData" border style="width: 100%" :max-height="420" :key="tableData.length">
-        <el-table-column prop="sbu" label="sbu" width="100px" align="center" ></el-table-column>
+        <el-table-column prop="sbu" label="sbu" width="100px" align="center"></el-table-column>
         <el-table-column prop="eqid" label="设备ID" width="240px" align="center" show-overflow-tooltip></el-table-column>
-        <el-table-column prop="t" label="实时温度" width="100px" align="center"  ></el-table-column>
-        <el-table-column prop="coefficient" label="系数" width="100px" align="center" ></el-table-column>
+
+        <el-table-column prop="t" label="实时温度" width="100px" align="center"></el-table-column>
+        <el-table-column prop="coefficient" label="温度系数" width="100px" align="center"></el-table-column>
         <el-table-column label="换算后的温度" width="120px" align="center">
           <template slot-scope="scope">
             {{ scope.row.t ? (scope.row.t * scope.row.coefficient).toFixed(2) : '' }}
           </template>
         </el-table-column>
+
+
+        <el-table-column prop="h" label="实时湿度" width="100px" align="center"></el-table-column>
+        <el-table-column prop="coefficientH" label="温度系数" width="100px" align="center"></el-table-column>
+        <el-table-column label="换算后的温度" width="120px" align="center">
+          <template slot-scope="scope">
+            {{ scope.row.h ? (scope.row.h * scope.row.coefficientH).toFixed(2) : '' }}
+          </template>
+        </el-table-column>
+
         <el-table-column label="操作" align="center">
           <template slot-scope="scope">
             <el-button @click="deleteRow(scope.$index)" type="danger" size="mini" plain>删除</el-button>
@@ -63,24 +98,120 @@
 
     <div class="form-buttom-container">
       <el-form :inline="true" class="demo-form-inline" :rules="rules" ref="form" v-model="form">
-        <el-form-item label="组名称">
-          <el-input v-model="form.groupName" style="width: 120px" placeholder="groupName"></el-input>
-        </el-form-item>
-        <el-form-item label="预期温度">
-          <el-input v-model="form.expMaxTem" placeholder="min" style="display: inline-block; width: 70px;"
-            @input="validateNumber('expMaxTem')">
-          </el-input>
-          <span style="display: inline-block; vertical-align: top;">C°</span>
-          <span style="display: inline-block; vertical-align: top; margin-inline: 10px;">-</span>
-          <el-input v-model="form.expMinTem" placeholder="max" style="display: inline-block; width: 70px;"
-            @input="validateNumber('expMinTem')">
-          </el-input>
-          <span style="display: inline-block; vertical-align: top;">C°</span>
-        </el-form-item>
-        <el-form-item label="温区实时温度" v-if="tableData.length != 0">
-          <el-tag type="success">{{ calculateAverageTemperature()}}C°</el-tag>
-        </el-form-item>
 
+        <el-row>
+          <el-col :span="6">
+            <el-form-item label="温区实时温度" v-if="tableData.length != 0">
+              <el-tag type="success">{{ calculateAverageTemperature() }}C°</el-tag>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="温区实时湿度" v-if="tableData.length != 0">
+              <el-tag type="success">{{ calculateAverageHumidity() }}%</el-tag>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="预期温度">
+              <el-input v-model="form.expMinTem" placeholder="min" style="display: inline-block; width: 70px;"
+                @input="validateNumber('expMinTem')"></el-input>
+              <span style="display: inline-block; vertical-align: top;">C°</span>
+              <span style="display: inline-block; vertical-align: top; margin-inline: 10px;">-</span>
+              <el-input v-model="form.expMaxTem" placeholder="max" style="display: inline-block; width: 70px;"
+                @input="validateNumber('expMaxTem')"></el-input>
+              <span style="display: inline-block; vertical-align: top;">C°</span>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="8">
+            <el-form-item label="预期湿度">
+              <el-input v-model="form.expMinH" placeholder="min" style="display: inline-block; width: 70px;"
+                @input="validateNumber('expMinH')"></el-input>
+              <span style="display: inline-block; vertical-align: top;">%</span>
+              <span style="display: inline-block; vertical-align: top; margin-inline: 10px;">-</span>
+              <el-input v-model="form.expMaxH" placeholder="max" style="display: inline-block; width: 70px;"
+                @input="validateNumber('expMaxH')"></el-input>
+              <span style="display: inline-block; vertical-align: top;">%</span>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="组名称">
+              <el-input v-model="form.groupName" style="width: 230px" placeholder="groupName"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+
+
+            <el-form-item label="空调设备">
+
+              <el-select v-model="form.appGroup.appId" placeholder="请选择设备" clearable filterable style="width: 170px">
+                <el-option v-for="airDevice in airDevices" :key="airDevice.appId" :label="airDevice.name"
+                  :value="airDevice.appId">
+                </el-option>
+              </el-select>
+
+
+
+              <!-- <el-input  v-model="form.appGroup.appId" style="width: 200px" placeholder="空调设备ID"></el-input> -->
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <!-- 早上 -->
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="早班">
+              <el-time-select placeholder="起始时间" v-model="form.mStartTime" :picker-options="{
+                start: '08:00',
+                step: '00:15',
+                end: '12:15'
+              }" style="width: 120px;"></el-time-select>
+              <el-time-select placeholder="结束时间" v-model="form.mEndTime" :picker-options="{
+                start: '08:00',
+                step: '00:15',
+                end: '12:15',
+                minTime: form.mStartTime
+              }" style="width: 120px;"></el-time-select>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="8">
+            <el-form-item label="午班">
+              <el-time-select placeholder="起始时间" v-model="form.aStartTime" :picker-options="{
+                start: '13:00',
+                step: '00:15',
+                end: '18:00'
+              }" style="width: 120px;"></el-time-select>
+              <el-time-select placeholder="结束时间" v-model="form.aEndTime" :picker-options="{
+                start: '13:00',
+                step: '00:15',
+                end: '18:00',
+                minTime: form.aStartTime
+              }" style="width: 120px;"></el-time-select>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="8">
+            <el-form-item label="晚班">
+              <el-time-select placeholder="起始时间" v-model="form.eStartTime" :picker-options="{
+                start: '18:00',
+                step: '00:15',
+                end: '22:00'
+              }" style="width: 120px;"></el-time-select>
+              <el-time-select placeholder="结束时间" v-model="form.eEndTime" :picker-options="{
+                start: '18:00',
+                step: '00:15',
+                end: '22:00',
+                minTime: form.eStartTime
+              }" style="width: 120px;"></el-time-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form>
     </div>
 
@@ -90,7 +221,6 @@
         v-if="groupId != '' && tableData.length != 0">确认修改组别</el-button>
       <el-button type="danger" @click="onSubmitTable" plain round size="medium "
         v-if="groupId != '' && tableData.length == 0">确认删除组别</el-button>
-
     </div>
 
   </div>
@@ -101,6 +231,13 @@ import { saveVisitLog } from '@/utils/commonUtils.js'
 export default {
   data() {
     return {
+
+      value: new Date(),
+      markedDates: [
+        new Date('2025-03-01'),
+        new Date('2025-03-05')
+      ],
+      workCalendarDigStatus: false,
       rules: {
         groupName: [
           { required: true, message: '请输入组名称', trigger: 'blur' }
@@ -109,6 +246,7 @@ export default {
       },
       groupId: '',
       devices: [],
+      airDevices: [],
       groupIds: [],
       sbu: [],
       tableData: [
@@ -119,10 +257,21 @@ export default {
         expMinTem: '',
         expMaxTem: '',
         coefficient: '',
+        coefficientH: '',
         sbu: '',
         id: '',
         t: '',
-        coefficientT: ''
+        h: '',
+        mStartTime: '',
+        mEndTime: '',
+        aStartTime: '',
+        aEndTime: '',
+        eStartTime: '',
+        eEndTime: '',
+        coefficientT: '',
+        appGroup: {
+          appId: ''
+        }
       }
     };
   },
@@ -149,6 +298,7 @@ export default {
 
   },
   created() {
+    this.getAppInfoList();
     this.getThRecord();
     this.getThKvRecordGroups();
     saveVisitLog('温度系数比值');
@@ -157,17 +307,55 @@ export default {
   },
   methods: {
 
-    calculateAverageTemperature() {
-    const totalConvertedTemperature = this.tableData.reduce((acc, row) => {
-      if (row.t && row.coefficient && row.t !== '' && row.coefficient !== '') {
-        acc += row.t * row.coefficient;
-      }
-      return acc;
-    }, 0);
 
-    const count = this.tableData.length;
-    return count > 0 ? (totalConvertedTemperature / count).toFixed(2) : '';
-  },
+    dayStyle(date) {
+      // 格式化日期，确保可以与集合中的日期匹配
+      const formattedDate = new Date(date).toLocaleDateString();
+      for (let markedDate of this.markedDates) {
+        if (new Date(markedDate).toLocaleDateString() === formattedDate) {
+          return {
+            backgroundColor: '#409EFF',  // 设置标记的日期的背景颜色
+            color: 'white',              // 设置字体颜色
+            borderRadius: '50%',         // 让勾选圆形
+          };
+        }
+      }
+      return {};
+    },
+  
+
+    workCalendar() {
+      this.workCalendarDigStatus = true;
+    },
+    calculateAverageTemperature() {
+      const totalConvertedTemperature = this.tableData.reduce((acc, row) => {
+        if (row.t && row.coefficient && row.t !== '' && row.coefficient !== '') {
+          acc += row.t * row.coefficient;
+        }
+        return acc;
+      }, 0);
+
+      const count = this.tableData.length;
+      return count > 0 ? (totalConvertedTemperature / count).toFixed(2) : '';
+    },
+
+    calculateAverageHumidity() {
+      const totalHumidity = this.tableData.reduce((acc, row) => {
+        // 确保 row.h 是有效的数字字符串
+        if (row.h && !isNaN(Number(row.h))) {
+          acc += Number(row.h * row.coefficientH);  // 将 row.h 转换为数字后累加
+        } else {
+          console.warn("Invalid row.h value:", row.h);  // 输出无效的值
+        }
+        return acc;
+      }, 0);
+
+      const count = this.tableData.length;
+      return count > 0 ? (totalHumidity / count).toFixed(2) : '';
+    }
+    ,
+
+
     validateNumber(field) {
       this.form[field] = this.form[field].replace(/[^0-9.]/g, ''); // Allow only numbers and decimal points
     },
@@ -177,11 +365,23 @@ export default {
       this.form = {
         expMinTem: '',
         expMaxTem: '',
+        expMinH: '',
+        expMaxH: '',
         coefficient: '',
+        coefficientH: '',
         groupName: '',
         sbu: '',
         id: '',
-        coefficientT: ''
+        coefficientT: '',
+        mStartTime: '',
+        mEndTime: '',
+        aStartTime: '',
+        aEndTime: '',
+        eStartTime: '',
+        eEndTime: '',
+        appGroup: {
+          appId: ''
+        }
       };
     },
     onSubmitTable() {
@@ -198,8 +398,18 @@ export default {
         tableData: this.tableData,
         expMaxTem: this.form.expMaxTem,
         expMinTem: this.form.expMinTem,
-        groupName: this.form.groupName
+        expMaxH: this.form.expMaxH,
+        expMinH: this.form.expMinH,
+        groupName: this.form.groupName,
+        appId: this.form.appGroup.appId,
+        mStartTime: this.form.mStartTime,
+        mEndTime: this.form.mEndTime,
+        aStartTime: this.form.aStartTime,
+        aEndTime: this.form.aEndTime,
+        eStartTime: this.form.eStartTime,
+        eEndTime: this.form.eEndTime,
       };
+      console.log("data", data);
       this.$http({
         url: this.$http.adornUrl('/extProject/addKV'),
         method: 'post',
@@ -247,25 +457,46 @@ export default {
       });
     },
 
+
+    getAppInfoList() {
+      const params = {
+      };
+      this.$http({
+        url: this.$http.adornUrl('/extProject/getAppInfoList'),
+        method: 'get',
+        params: params,
+      }).then((response) => {
+        const data = response.data.data;
+        if (data) {
+          this.airDevices = data.map(item => ({
+            appId: item.appId,
+            name: item.name,
+          }));
+        }
+      }).catch((error) => {
+        console.log('error', error);
+      });
+    },
+
     getThRecord2(eqid) {
-  const params = { eqid: eqid };
-  return new Promise((resolve, reject) => {
-    this.$http({
-      url: this.$http.adornUrl('/extProject/getThRecord2'),
-      method: 'get',
-      params: params,
-    })
-    .then((response) => {
-      const data = response.data.data;
-      console.log("response", data);
-      resolve(data);  // Resolve the promise with the data
-    })
-    .catch((error) => {
-      console.log('error', error);
-      reject(error);  // Reject the promise with the error
-    });
-  });
-},
+      const params = { eqid: eqid };
+      return new Promise((resolve, reject) => {
+        this.$http({
+          url: this.$http.adornUrl('/extProject/getThRecord2'),
+          method: 'get',
+          params: params,
+        })
+          .then((response) => {
+            const data = response.data.data;
+            console.log("response", data);
+            resolve(data);  // Resolve the promise with the data
+          })
+          .catch((error) => {
+            console.log('error', error);
+            reject(error);  // Reject the promise with the error
+          });
+      });
+    },
 
 
 
@@ -285,7 +516,6 @@ export default {
           groupName: item.groupName
         }));
 
-        console.log("groupIds", this.groupIds)
         this.groupIds = [...new Map(this.groupIds.map(item => [item.groupId, item])).values()];
 
       }).catch((error) => {
@@ -308,9 +538,17 @@ export default {
         this.tableData = data
         this.form.expMaxTem = data[0].expMaxTem;
         this.form.expMinTem = data[0].expMinTem;
+        this.form.expMaxH = data[0].expMaxH;
+        this.form.expMinH = data[0].expMinH;
         this.form.groupName = data[0].groupName;
         this.form.coefficientT = data[0].coefficientT;
-
+        this.form.mStartTime = data[0].mStartTime;
+        this.form.mEndTime = data[0].mEndTime;
+        this.form.aStartTime = data[0].aStartTime;
+        this.form.aEndTime = data[0].aEndTime;
+        this.form.eStartTime = data[0].eStartTime;
+        this.form.eEndTime = data[0].eEndTime;
+        this.form.appGroup = data[0].appGroup;
       }).catch((error) => {
         console.log('Error:', error);
       });
@@ -318,21 +556,21 @@ export default {
 
 
 
-    async  onSubmit() {
-      if (!this.form.coefficient || !this.form.sbu || !this.form.id) {
+    async onSubmit() {
+      if (!this.form.coefficientH || !this.form.coefficient || !this.form.sbu || !this.form.id) {
         this.$message.error('请填写完整的表单数据');
         return;
       }
       const data = await this.getThRecord2(this.form.id);
-      console.info("data", data);
-      console.info("data",  data.t);
 
       this.tableData.push({
         sbu: this.form.sbu,
         eqid: this.form.id,
         id: this.form.id,
         t: data.t,
-        coefficient: this.form.coefficient
+        h: data.h,
+        coefficient: this.form.coefficient,
+        coefficientH: this.form.coefficientH
       });
       // 延迟操作，确保表格渲染完成后再执行其他操作
       this.$nextTick(() => {
@@ -369,13 +607,12 @@ export default {
 
 .form-container {
   text-align: center;
-
-  width: 60%;
-  margin: 40px auto;
+  width: 100%;
+  margin: 20px auto;
 }
 
 .table-container {
-  max-width: 40%;
+  max-width: 60%;
   margin: 20px auto;
 }
 
@@ -391,5 +628,20 @@ export default {
   margin: 20px auto;
   margin-top: 40px;
 
+
+}
+
+.el-row {
+  display: flex;
+  justify-content: center;
+  /* 水平居中 */
+  align-items: center;
+  /* 垂直居中 */
+}
+
+.el-col {
+  display: flex;
+  justify-content: center;
+  /* 居中列内的内容 */
 }
 </style>
