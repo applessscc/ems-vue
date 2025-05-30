7a224f4fb2687add8c84cfd5d0fcc7ebdb67d7ea
郑志宏 <2696122250@qq.com>
2025-05-30 15:50:27 +0800

    【代码提交】

--- src/views/modules/extProject/video-naintenance.vue
+++ src/views/modules/extProject/video-naintenance.vue
@@ -2,22 +2,20 @@
     <div>
         <el-form :inline="true" :model="{ searchForm: searchForm }" class="search-form">
             <el-form-item label="">
-                <el-input v-model="searchForm" placeholder="请输入appId/name" style="width: 300px;"></el-input>
+                <el-input v-model="searchForm" placeholder="请输入videoId/name" style="width: 300px;"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="fetchAppInfo()" plain>查询</el-button>
+                <el-button type="primary" @click="fetchVideoInfo()" plain>查询</el-button>
             </el-form-item>
             <el-form-item>
-                <el-button type="success" @click="addAppInfo()" plain>新增</el-button>
+                <el-button type="success" @click="addVideoInfo()" plain>新增</el-button>
             </el-form-item>
         </el-form>
-        <el-table :data="appInfoList" style="width: 100%" border>
-
-
-            <el-table-column label="appId" prop="appId" width="150">
+        <el-table :data="videoInfoList" style="width: 100%" border>
+            <el-table-column label="videoId" prop="videoId" width="150">
                 <template slot-scope="scope">
-                    {{ scope.row.ip == "" &&  scope.row.address == ""?"":scope.row.ip+"+"+scope.row.address }}
-                </template> 
+                    {{ scope.row.ip == "" && scope.row.address == "" ? "" : scope.row.ip + "+" + scope.row.address }}
+                </template>
             </el-table-column>
 
             <el-table-column label="name" prop="name" width="150">
@@ -45,16 +43,12 @@
             <el-table-column label="updateTime" prop="updateTime" width="180"></el-table-column>
             <el-table-column label="操作">
                 <template slot-scope="scope">
-                    <el-button type="warning" plain @click="handleUpdate(scope.row)" v-if="scope.row.id != null">修改
-                    </el-button>
-                    <el-button type="success" plain @click="handleUpdate(scope.row)" v-if="scope.row.id == null">新增
-                    </el-button>
+                    <el-button type="warning" plain @click="handleUpdate(scope.row)" v-if="scope.row.id != null">修改</el-button>
+                    <el-button type="success" plain @click="handleUpdate(scope.row)" v-if="scope.row.id == null">新增</el-button>
                     <el-button type="danger" plain @click="handleDelete(scope.row)">删除</el-button>
                 </template>
             </el-table-column>
-
         </el-table>
-
     </div>
 </template>
 
@@ -62,53 +56,46 @@
 export default {
     data() {
         return {
-            appInfoList: [], // This will hold the list of app info objects
+            videoInfoList: [], // 视频信息列表
             searchForm: "",
             dialogFormVisible: false,
-            newApp: {
-                id: null, // 新设备信息的ID，新增时为null
-                appId: '',
+            newVideo: {
+                id: null,
+                videoId: '',
                 name: '',
                 sap: '',
                 ip: '',
                 address: ''
-            }, // 新设备信息
+            }
         };
     },
     mounted() {
     },
     created() {
-        this.fetchAppInfo();
-    },
-    computed: {
-
+        this.fetchVideoInfo();
     },
     methods: {
-        addAppInfo() {
-            this.appInfoList.unshift({
-                id: null, // 新设备信息的ID，新增时为null
-                appId: this.newApp.appId,
-                name: this.newApp.name,
-                sap: this.newApp.sap,
-                ip: this.newApp.ip,
-                address: this.newApp.address,
+        addVideoInfo() {
+            this.videoInfoList.unshift({
+                id: null,
+                videoId: this.newVideo.videoId,
+                name: this.newVideo.name,
+                sap: this.newVideo.sap,
+                ip: this.newVideo.ip,
+                address: this.newVideo.address,
                 operator: this.$store.state.user.name,
                 createTime: this.formatDate(new Date()),
                 updateTime: this.formatDate(new Date())
             });
-
             this.$forceUpdate();
         },
-
-
-
         handleUpdate(row) {
             this.$http({
-                url: this.$http.adornUrl('/extProject/addAppInfo'),
+                url: this.$http.adornUrl('/extProject/addVideoInfo'),
                 method: 'post',
                 data: {
-                    id: row.id, // 新设备信息的ID，新增时为null
-                    appId: row.appId,
+                    id: row.id,
+                    videoId: row.videoId,
                     name: row.name,
                     sap: row.sap,
                     ip: row.ip,
@@ -124,20 +111,20 @@ export default {
                     } else {
                         this.$message.success('修改成功');
                     }
-                    this.fetchAppInfo();
+                    this.fetchVideoInfo();
                 } else {
                     this.$message.error(response.data.msg);
                 }
             }).catch((error) => {
                 console.error(error);
-            })
+            });
         },
         handleDelete(row) {
             if (row.id == null) {
-                this.appInfoList.splice(this.appInfoList.indexOf(row), 1);
+                this.videoInfoList.splice(this.videoInfoList.indexOf(row), 1);
             } else {
                 this.$http({
-                    url: this.$http.adornUrl('/extProject/deleteAppInfo'),
+                    url: this.$http.adornUrl('/extProject/deleteVideoInfo'),
                     method: 'get',
                     params: {
                         id: row.id
@@ -145,32 +132,31 @@ export default {
                 }).then((response) => {
                     if (response.data.code === 200) {
                         this.$message.success('删除成功');
-                        this.fetchAppInfo();
+                        this.fetchVideoInfo();
                     } else {
                         this.$message.error(response.data.msg);
                     }
                 }).catch((error) => {
                     console.error(error);
-                })
+                });
             }
         },
-        fetchAppInfo() {
+        fetchVideoInfo() {
             this.$http({
-                url: this.$http.adornUrl('/extProject/getAppInfoList'),
+                url: this.$http.adornUrl('/extProject/getVideoInfoList'),
                 method: 'get',
                 params: {
                     searchForm: this.searchForm
                 }
-
             }).then((response) => {
                 if (response.data.code === 200) {
-                    this.appInfoList = response.data.data;
+                    this.videoInfoList = response.data.data;
                 } else {
                     this.$message.error(response.data.msg);
                 }
             }).catch((error) => {
                 console.error(error);
-            })
+            });
         },
         formatDate(date) {
             if (!date) return '';
@@ -181,14 +167,11 @@ export default {
             const hours = d.getHours().toString().padStart(2, '0');
             const minutes = d.getMinutes().toString().padStart(2, '0');
             const seconds = d.getSeconds().toString().padStart(2, '0');
-            return `
-            ${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+            return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
         },
-
         resetForm() {
-            // 在关闭对话框时重置表单数据
             this.$nextTick(() => {
-                this.$refs.newAppForm.resetFields();
+                this.$refs.newVideoForm.resetFields();
             });
         }
     }
--- src/views/modules/extProject/video.vue
+++ src/views/modules/extProject/video.vue
@@ -10,20 +10,54 @@
         <video ref="thermalPlayer" autoplay muted playsinline></video>
       </div>
     </div>
+
     <div class="button-group">
-      <el-button type="success" plain @click="startCamera" v-if="startCameraStatus">连接摄像头</el-button>
-      <el-button type="danger" plain @click="stopCamera" v-if="stopCameraStatus">断开摄像头连接</el-button>
+      <div v-if="startCameraStatus" class="form-wrapper">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="name" style="width: 250px;">
+            <el-select v-model="videoInfo" placeholder="请选择摄像头名称" :clearable="true">
+              <el-option v-for="item in videoInfoList" :key="item.name" :label="item.name" :value="item">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="ip" style="width: 250px;">
+            <el-input v-model="form.ip" placeholder="请输入摄像头IP地址"></el-input>
+          </el-form-item>
+          <el-form-item label="password" style="width: 250px;">
+            <el-input v-model="form.password" placeholder="请输入密码" show-password></el-input>
+          </el-form-item>
+          <el-form-item style="width: 250px;">
+            <el-button type="success" plain @click="startCamera">连接摄像头</el-button>
+          </el-form-item>
+        </el-form>
+
+      </div>
+
+      <div v-if="stopCameraStatus" class="form-wrapper">
+        <el-button type="danger" plain @click="stopCamera">断开摄像头连接</el-button>
+      </div>
+
     </div>
+
   </div>
 </template>
 
 <script>
 import Hls from 'hls.js';
+import { watch } from 'less';
 
 export default {
   name: 'LiveVideo',
   data() {
     return {
+      videoInfo: {},
+      form: {
+        name: '',
+        ip: '',
+        password: ''
+      },
+      videoInfoList: [],
+      searchForm: '',
       startCameraStatus: false,
       stopCameraStatus: false,
       m3u8Url: this.$http.adornUrl(`/videos/stream.m3u8`),
@@ -39,16 +73,29 @@ export default {
     }
   },
   mounted() {
-
+    this.fetchVideoInfo();
     this.getActiveStreams();  // 先调用一次
     this.intervalId = setInterval(() => {
       this.getActiveStreams();
-    }, 10000);  // 10秒
+      this.fetchVideoInfo();
+
+    }, 15000);  // 10秒
     this.initPlayer(this.$refs.videoPlayer, this.m3u8Url);
     this.initPlayer(this.$refs.thermalPlayer, this.thermalUrl);
   },
+
+  watch: {
+    videoInfo(newVal) {
+      this.form.ip = newVal.ip || '';
+      this.form.password = newVal.password || '';
+      this.form.name = newVal.name || '';
+    }
+  },
+
   methods: {
+
     initPlayer(videoElement, url) {
+
       if (Hls.isSupported()) {
         const hls = new Hls({
           startPosition: -1,        // 默认直播行为，从最新位置开始播放
@@ -73,14 +120,27 @@ export default {
     startCamera() {
       this.$http({
         url: this.$http.adornUrl(`/extProject/startVideo`),
-        method: 'get'
-      }).then(() => {
-        this.$message({
-          message: '摄像头已连接',
-          type: 'success',
-          duration: 800
-        });
+        method: 'get',
+        params: {
+          ip: this.form.ip,
+          password: this.form.password,
+        }
+      }).then((response) => {
+        if (response.data.code === 200) {
+          this.$message({
+            message: '摄像头已连接',
+            type: 'success',
+            duration: 800
+          });
+        } else {
+          this.$message({
+            message: '摄像头连接失败！',
+            type: 'error',
+            duration: 1000
+          });
+        }
         this.getActiveStreams();
+
       }).catch(() => {
         this.$message({
           message: '连接摄像头失败',
@@ -130,7 +190,25 @@ export default {
       }).catch((error) => {
         console.error('getActiveStreams', error);
       });
-    }
+    },
+
+    fetchVideoInfo() {
+      this.$http({
+        url: this.$http.adornUrl('/extProject/getVideoInfoList'),
+        method: 'get',
+        params: {
+          searchForm: this.searchForm
+        }
+      }).then((response) => {
+        if (response.data.code === 200) {
+          this.videoInfoList = response.data.data;
+        } else {
+          this.$message.error(response.data.msg);
+        }
+      }).catch((error) => {
+        console.error(error);
+      });
+    },
 
   }
 };
@@ -140,7 +218,7 @@ export default {
 .button-group {
   text-align: center;
   margin: 30px 0;
-  margin-top: 90px;
+  margin-top: 50px;
 }
 
 .button-group .el-button:first-child {
@@ -168,5 +246,10 @@ video {
   width: 100%;
   height: auto;
   display: block;
+}
+
+.form-wrapper {
+  display: flex;
+  justify-content: center;
 }
 </style>
